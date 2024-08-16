273acb47b3a90f229f929ec65d8ca9522cc4f03a
burakiscoding <burak.kurtarir35@hotmail.com>
2024-08-16 16:45:34 +0300

    Initial Commit

---
+++ NavigationExample/NavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExampleApp.swift
+//  NavigationExample
+//
+//  Created by Burak Kurtarır on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
